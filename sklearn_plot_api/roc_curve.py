3322da127b5d5929416e7b461655c6842dc6b688
Thomas Fan <thomasjpfan@gmail.com>
2019-07-11 10:49:52 -0500

    ENH Update to roc_curve

--- sklearn_plot_api/roc_curve.py
+++ sklearn_plot_api/roc_curve.py
@@ -2,54 +2,161 @@ from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
 
 
-class RocCurveViz:
-    def _create(self, est, X, y, response_method="auto", pos_label=None):
-        predict_proba = getattr(est, 'predict_proba', None)
-        decision_function = getattr(est, 'decision_function', None)
+class RocCurveVisualizer:
+    """ROC Curve visualization
 
-        if response_method == 'auto':
-            prediction_method = predict_proba or decision_function
-        elif response_method == 'predict_proba':
-            prediction_method = predict_proba
-        elif response_method == 'decision_function':
-            prediction_method = decision_function
-        if prediction_method is None:
-            raise ValueError("Estimator does not have proper response method")
+    Parameters
+    ----------
 
+    estimator : estimator instance
+        Trained classifier.
+
+    X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        Input values.
+
+    y : array-like, shape (n_samples,)
+        Target values.
+
+    pos_label : int str or None, optional(default=None)
+        The label of the positive class.
+        When ``pos_label=None``, if y_true is in {-1, 1} or {0, 1},
+        ``pos_label`` is set to 1, otherwise an error will be raised.
+
+    sample_weight : array-like of shape = [n_samples], optional
+        Sample weights.
+
+    drop_intermediate : boolean, optional (default=True)
+        Whether to drop some suboptimal thresholds which would not appear
+        on a plotted ROC curve. This is useful in order to create lighter
+        ROC curves.
+
+    response_method : 'predict_proba' or 'decision_function' optional \
+    (default='predict_proba')
+        Method to call estimator to get target scores
+
+    Attributes
+    ----------
+    fpr_ : ndarray
+        False positive rate.
+    tpr_ : ndarray
+        True positive rate.
+    label_ : string
+        Name of estimator.
+    line_ : matplotlib Artist
+        ROC Curve.
+    ax_ : matplotlib Axes
+        Axes with roc curve
+    figure_ : matplotlib Figure
+        Figure containing the curve
+    """
+
+    def __init__(self,
+                 estimator,
+                 X,
+                 y,
+                 *,
+                 pos_label=None,
+                 sample_weight=None,
+                 drop_intermediate=True,
+                 response_method="predict_proba"):
+        """Computes and stores values needed for visualization"""
+
+        prediction_method = getattr(estimator, response_method)
         y_pred = prediction_method(X)
-        if y_pred.ndim == 2:
+
+        if y_pred.ndim != 1:
+            if y_pred.shape[1] > 2:
+                raise ValueError("Estimator must be a binary classifier")
             y_pred = y_pred[:, 1]
-        fpr, tpr, _ = roc_curve(y, y_pred, pos_label=pos_label)
+        fpr, tpr, _ = roc_curve(y,
+                                y_pred,
+                                pos_label=pos_label,
+                                drop_intermediate=drop_intermediate)
 
         self.fpr_ = fpr
         self.tpr_ = tpr
-        self.label_ = est.__class__.__name__
-        return self
+        self.label_ = estimator.__class__.__name__
 
-    def plot(self, ax=None, viz=None):
-        if viz is not None:
-            ax = viz.ax_
-        elif ax is None:
-            fig = plt.figure()
-            ax = fig.add_subplot()
+    def plot(self, ax=None):
+        """Plot visualization
+
+        Parameters
+        ----------
+        ax : Matplotlib Axes, optional (default=None)
+            axes object to plot on
+        """
+        check_matplotlib_support('plot_roc_curve')  # noqa
+        import matplotlib.pyplot as plt  # noqa
+
+        if ax is None:
+            fig, ax = plt.subplots()
 
         self.line_ = ax.plot(self.fpr_, self.tpr_, label=self.label_)[0]
         ax.set_xlabel('False Positive Rate')
         ax.set_ylabel('True Positive Rate')
+
         self.ax_ = ax
         self.figure_ = ax.figure
-
         return self
 
-    @classmethod
-    def _plot(cls, est, X, y, response_method="auto", pos_label=None, ax=None):
-        new_viz = cls()._create(est,
-                                X,
-                                y,
-                                response_method=response_method,
-                                pos_label=pos_label)
-        new_viz.plot(ax=ax)
-        return new_viz
 
+def plot_roc_curve(estimator,
+                   X,
+                   y,
+                   pos_label=None,
+                   sample_weight=None,
+                   drop_intermediate=True,
+                   response_method="predict_proba",
+                   ax=None):
+    """Plot Receiver operating characteristic (ROC) curve
 
-plot_roc_curve = RocCurveViz._plot
+    Note: this implementation is restricted to the binary classification task.
+
+    Read more in the :ref:`User Guide <roc_metrics>`.
+
+    Parameters
+    ----------
+
+    estimator : estimator instance
+        Trained classifier.
+
+    X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        Input values.
+
+    y : array-like, shape (n_samples,)
+        Target values.
+
+    pos_label : int or str, default=None
+        The label of the positive class.
+        When ``pos_label=None``, if y_true is in {-1, 1} or {0, 1},
+        ``pos_label`` is set to 1, otherwise an error will be raised.
+
+    sample_weight : array-like of shape = [n_samples], optional (default=None)
+        Sample weights.
+
+    drop_intermediate : boolean, optional (default=True)
+        Whether to drop some suboptimal thresholds which would not appear
+        on a plotted ROC curve. This is useful in order to create lighter
+        ROC curves.
+
+    response_method : 'predict_proba' or 'decision_function' optional \
+    (default='predict_proba')
+        Method to call estimator to get target scores
+
+    ax : matplotlib axes, optional (default=None)
+        axes object to plot on
+
+    Returns
+    -------
+    viz : :class:`sklearn.metrics.plot.RocCurveVisualizer`
+        object that stores computed values
+    """
+    viz = RocCurveVisualizer(estimator,
+                             X,
+                             y,
+                             sample_weight=sample_weight,
+                             pos_label=pos_label,
+                             drop_intermediate=drop_intermediate,
+                             response_method=response_method)
+    viz.plot(ax=ax)
+    return viz
